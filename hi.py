65c79c0552c9d524461205cf42a3d272c185a9b0
Soundhar004 <soundhar007k@gmail.com>
2024-02-16 20:03:56 +0530

    modified now

--- hi.py
+++ hi.py
@@ -1,25 +1,25 @@
-# def add(x,y):
-#     z=x+y
-#     return z
-# def sub(x,y):
-#     z=x-y
-#     return z
+ def add(x,y):
+     z=x+y
+     return z
+ def sub(x,y):
+     z=x-y
+     return z
 
 
 
-# while True:
-#     a=int(input("enter the a value : "))
-#     b=int(input("enter the b value : "))
-#     c=add(a,b)
-#     f=sub(a,b)
-#     print("your add value : {}".format(c))    
-#     print("your sub value : {}".format(f))
+ while True:
+     a=int(input("enter the a value : "))
+     b=int(input("enter the b value : "))
+     c=add(a,b)
+     f=sub(a,b)
+     print("your add value : {}".format(c))    
+     print("your sub value : {}".format(f))
 
-#     a=input("if you want to continue press yes otherwise no : ")
-#     if a=="yes":
-#         continue
-#     else:
-#         break
+     a=input("if you want to continue press yes otherwise no : ")
+     if a=="yes":
+         continue
+     else:
+         break
 
 
 
